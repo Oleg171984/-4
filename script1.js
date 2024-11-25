01b595cfffcd82cff1eb7591c913a63cd2993fca
Oleg171984 <kucugurnyj171984@gmail.com>
2024-11-25 18:05:59 +0200

    made changes and js

--- script1.js
+++ script1.js
@@ -1,12 +1,8 @@
-let nameUser
-while (true) {
-    nameUser = prompt('I am waiting your name')
-    if (nameUser) {
-        alert(`Hello ,${nameUser}! How are you?`)
-        break;
-    } else {
-        
-        alert('Pliase enter name again')
-    }
+let  nameUser = prompt('I am waiting your name')
+if (nameUser) {
+    alert(`Hello ,${nameUser}! How are you?`)
+} else {
+    alert('Pliase enter name again')
 }
+
 
